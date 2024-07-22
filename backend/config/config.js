cba4df7a6c4cd8e80c3c7086ec61f0237e32676f
Krishna Srivastava <7824krishna@gmail.com>
2024-07-22 07:05:27 +0530

    First commit

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,4 +1,6 @@
+require('dotenv').config()
+
 module.exports = {
-    JWT_SECRET: "KrishTaskPlanner_WebApp",
-    MONGO_URL: "mongodb+srv://krishna:mongodb@cluster0.sxljdbv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/todos"
+    JWT_SECRET:process.env.JWT_SECRET,
+    MONGO_URL:process.env.MONGO_URL
 }
